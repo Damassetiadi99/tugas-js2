b438e9ebb9810797455a03548e062d59cf8f8f4c
damassetiadi99 <damassetiadi999@gmail.com>
2023-07-10 00:28:08 +0700

    lanjutan

--- tugas1.js
+++ tugas1.js
@@ -1,36 +1,44 @@
-const fruits = ['apple', 'banana', 'cherry']
-fruits.slice(0, 2)
-// console.log(fruits)
-// outputnya ['apple', 'banana']
+            //number 1 method slice
+// const Hobbies = ['Reading', 'Badminton', 'Healing','coding','jajan']
+// Hobbies.slice()
+// console.log(Hobbies.slice(1,4))
 
-//method 2
-const myNum = [1, 2, 3]
-myNum.push(4);  
-console.log(myNum)
-// outputnya : [1, 2, 3, 4]
-//methode 3"pop" menghapus array dari belakang 
-const myNum = [1, 2, 3]
-myNum.pop()
-console.log(myNum)
-// outputnya : [1, 2]
-// nmr 4
+
+// outputnya ['reading', 'Badminton','Healing']
+
+                //number 2 method push/ menambahkan element padaarray dari belakang
+// const myNilai = [80, 90, 65]
+// myNilai.push(90,40);  
+// console.log(myNilai)
+// outputnya : [80, 90, 65, 90,40]
+
+
+
+         //number 3 methode"pop" / menghapus array dari belakang 
+// const favoritFood = ['sego Liwet','thengkleng','gudeg','bakso']
+// favoritFood.pop()
+// console.log(favoritFood)
+// outputnya : ['sego liwet','thengkleng','gudeg']
+
+        // nmr 4 method "shift" / menghapus array dari depan
+// const  favoritFruits = ['apple','oranges','mangostin','grape']
+// favoritFruits.shift();
+// console.log(favoritFruits)
+// outputnya : ['oranges','mangostin','grape']
+
+                // nmr 5 method unshift/ menambahkan elemen pada array dari depan
+const favoritDrink = ['mineral water', 'orange juice']
+favoritDrink.unshift('avocado juice', 'milk');
+console.log(favoritDrink)
+// // outputnya : ['avocado juice', 'milk','mineral water', 'orange juice' ]
+
+            // nmr 6 method "shift"/
 const myNum = [1, 2, 3]
 myNum.shift();
 console.log(myNum)
-// outputnya : [2, 3]
-// nmr 5
-const myNum = [3, 4]
-myNum.unshift(1, 2);
-console.log(myNum)
-// outputnya : [1, 2, 3, 4]
+outputnya : [2, 3]
 
-// nmr 6
-const myNum = [1, 2, 3]
-myNum.shift();
-console.log(myNum)
-// outputnya : [2, 3]
-
-// nmr 7
+            // nmr 7
 const debian = ['ubuntu', 'mint', 'kali']
 const redhat = ['rhel', 'centos']
 const arch = ['manjaro', 'chakra']
@@ -38,12 +46,13 @@ const linuxDistro = debian.concat(redhat, arch)
 console.log(linuxDistro)
 // output : ['ubuntu', 'mint', 'kali', 'rhel', 'centos', 'manjaro', 'chakra' ]
 
-// nmr 8
-onst fruits = ['apple', 'banana', 'cherry']
-fruits.splice(0, 1) // menghapus 1 item di index 0
-console.log(fruits) // ['banana', 'cherry']
+// // nmr 8
+// const fruits = ['apple', 'banana', 'cherry']
+// fruits.splice(0, 1) // menghapus 1 item di index 0
+// // console.log(fruits) // ['banana', 'cherry']
 
-// nmr 9
-const fruits = ['apple', 'banana', 'cherry']
-fruits.indexOf('apple') // return 0
-fruits.indexOf('mango') // return -1
+// // nmr 9
+// const fruits = ['apple', 'banana', 'cherry']
+// fruits.indexOf('apple') // return 0
+// fruits.indexOf('mango') // return -1
+// // console.log(fruits)
