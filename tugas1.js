37d4131e0db0f375e0e415dab816ba060ae70f62
damassetiadi99 <damassetiadi999@gmail.com>
2023-07-11 00:13:52 +0700

    fix

--- tugas1.js
+++ tugas1.js
@@ -1,24 +1,24 @@
             //number 1 method slice
 // const Hobbies = ['Reading', 'Badminton', 'Healing','coding','jajan']
-// Hobbies.slice()
-// console.log(Hobbies.slice(1,4))
+// Hobbies.slice(0,5)
+// console.log(Hobbies.slice(2,4))
 
 
 // outputnya ['reading', 'Badminton','Healing']
 
-                //number 2 method push/ menambahkan element padaarray dari belakang
+//number 2 method push/ menambahkan element padaarray dari belakang
 // const myNilai = [80, 90, 65]
 // myNilai.push(90,40);  
 // console.log(myNilai)
-// outputnya : [80, 90, 65, 90,40]
+// // outputnya : [80, 90, 65, 90,40]
 
 
 
          //number 3 methode"pop" / menghapus array dari belakang 
-// const favoritFood = ['sego Liwet','thengkleng','gudeg','bakso']
-// favoritFood.pop()
-// console.log(favoritFood)
-// outputnya : ['sego liwet','thengkleng','gudeg']
+const favoritFood = ['sego Liwet','thengkleng','gudeg','bakso']
+favoritFood.pop()
+console.log(favoritFood)
+// // outputnya : ['sego liwet','thengkleng','gudeg']
 
         // nmr 4 method "shift" / menghapus array dari depan
 // const  favoritFruits = ['apple','oranges','mangostin','grape']
@@ -26,33 +26,40 @@
 // console.log(favoritFruits)
 // outputnya : ['oranges','mangostin','grape']
 
-                // nmr 5 method unshift/ menambahkan elemen pada array dari depan
-const favoritDrink = ['mineral water', 'orange juice']
-favoritDrink.unshift('avocado juice', 'milk');
-console.log(favoritDrink)
-// // outputnya : ['avocado juice', 'milk','mineral water', 'orange juice' ]
+  // nmr 5 method unshift/ menambahkan elemen pada array dari depan
+// const favoritDrink = ['mineral water', 'orange juice']
+// favoritDrink.unshift('avocado juice', 'milk');
+// console.log(favoritDrink)
+// outputnya : ['avocado juice', 'milk','mineral water', 'orange juice' ]
 
             // nmr 6 method "shift"/
-const myNum = [1, 2, 3]
-myNum.shift();
-console.log(myNum)
-outputnya : [2, 3]
+// const myNum = [1, 2, 3]
+// myNum.shift();
+// console.log(myNum)
+// outputnya : [2, 3]
 
             // nmr 7
-const debian = ['ubuntu', 'mint', 'kali']
-const redhat = ['rhel', 'centos']
-const arch = ['manjaro', 'chakra']
-const linuxDistro = debian.concat(redhat, arch)
-console.log(linuxDistro)
+// const debian = ['ubuntu', 'mint', 'kali']
+// const redhat = ['rhel', 'centos']
+// const arch = ['manjaro', 'chakra']
+// const linuxDistro = debian.concat(redhat, arch)
+// console.log(linuxDistro)
 // output : ['ubuntu', 'mint', 'kali', 'rhel', 'centos', 'manjaro', 'chakra' ]
 
-// // nmr 8
+                // nmr 8 splice
 // const fruits = ['apple', 'banana', 'cherry']
 // fruits.splice(0, 1) // menghapus 1 item di index 0
 // // console.log(fruits) // ['banana', 'cherry']
 
-// // nmr 9
+                // nmr 9
 // const fruits = ['apple', 'banana', 'cherry']
 // fruits.indexOf('apple') // return 0
 // fruits.indexOf('mango') // return -1
 // // console.log(fruits)
+
+
+        //nmr 10 includes
+// const fruits = ['apple', 'banana', 'cherry']
+// fruits.includes('apple') // return true
+// fruits.includes('mango') // return false
+// console.log(fruits)
--- tugas3.js
+++ tugas3.js
@@ -4,7 +4,7 @@ const seleksiNilai = (nilaiAwal, nilaiAkhir, dataArray) => {
 
             let sortir = dataArray.filter((hasil) => hasil > nilaiAwal && hasil < nilaiAkhir)
             if (sortir.length > 0) {
-                console.log(filter.sort((a,b)=>a-b))
+                console.log(sortir.sort((a,b)=>a-b))
             } else {
                 console.log('nilai tidak ditemukan')
             }
@@ -15,4 +15,7 @@ const seleksiNilai = (nilaiAwal, nilaiAkhir, dataArray) => {
         console.log('nilai akhir harus lebih besar dari nilai awal')
     }
 }
+seleksiNilai(5, 20 , [2, 25, 4, 14, 17, 30, 8])
+seleksiNilai(15, 3 , [2, 25, 4, 14, 17, 30, 8])
+seleksiNilai(5, 17 , [2, 25, 4])
 seleksiNilai(5, 17 , [2, 25, 4, 1, 30, 18])
